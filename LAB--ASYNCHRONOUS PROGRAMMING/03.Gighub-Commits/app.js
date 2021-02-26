97b2b09e4ec42d2941f279e8f66b1bb8d81b52ac
Yordan Nikolov <65885882+nlotoo@users.noreply.github.com>
2021-02-26 18:02:34 +0000

    Update app.js

--- LAB--ASYNCHRONOUS PROGRAMMING/03.Gighub-Commits/app.js
+++ LAB--ASYNCHRONOUS PROGRAMMING/03.Gighub-Commits/app.js
@@ -1,4 +1,33 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let userName = document.querySelector("#username").value
+    let repo = document.querySelector("#repo").value
+
+    const url = `https://api.github.com/repos/${userName}/${repo}/commits`
+
+    fetch(url)
+        .then(r => r.json())
+        .then(r => headError)
+        .then(data => {
+
+            document.querySelector("#commits").innerHTML = ''
+
+            data.forEach(ele => {
+                let autorName = ele.commit['author']['name']
+                let authorMessage = ele.commit['message']
+                let li = document.createElement('li')
+                li.textContent = `${autorName}: ${authorMessage}`
+                document.querySelector("#commits").appendChild(li)
+            });
+        })
+        .catch(err => {
+            document.querySelector("#commits").innerHTML += err
+        })
+
+
+}
+function headError(r) {
+    if (r.status === 404) {
+        throw new Error(`Error : ${r.status} ( ${r.statusText} )`)
+    }
+    return r
 }
