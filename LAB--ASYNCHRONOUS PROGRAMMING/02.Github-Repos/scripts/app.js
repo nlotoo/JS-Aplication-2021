55fac8768daf0eab8e710a23e0b8608a9fa06548
Yordan Nikolov <65885882+nlotoo@users.noreply.github.com>
2021-02-26 00:15:18 +0000

    Update app.js

--- LAB--ASYNCHRONOUS PROGRAMMING/02.Github-Repos/scripts/app.js
+++ LAB--ASYNCHRONOUS PROGRAMMING/02.Github-Repos/scripts/app.js
@@ -3,14 +3,25 @@ function loadRepos() {
 
 	let url = `https://api.github.com/users/${username}/repos`
 
-
 	fetch(url)
 		.then(r => r.json())
 		.then(data => {
+			document.querySelector("#repos").innerHTML = ''
 			data.forEach(e => {
-				console.log(e.id)
+				console.log(e)
+				let clone_url = e.clone_url
+				let curent = `${username}/${e.name}`
+				let li = document.createElement('li')
+				let a = document.createElement('a')
+				a.textContent = curent
+				a.href = `${clone_url}`
+			
+				li.appendChild(a)
+				document.querySelector("#repos").appendChild(li)
 			});
 		})
-
+		.catch((error) => {
+			console.error(error);
+		});
 
 }
